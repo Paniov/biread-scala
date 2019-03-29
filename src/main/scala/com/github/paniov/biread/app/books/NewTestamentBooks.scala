4b777aa721921d609bb88aa5ed9691850a72f29f
paniov <paniov@gmail.com>
2019-03-29 13:07:31 -0700

    Add tests for chapter's parts

--- src/main/scala/com/github/paniov/biread/app/books/NewTestamentBooks.scala
+++ src/main/scala/com/github/paniov/biread/app/books/NewTestamentBooks.scala
@@ -57,62 +57,62 @@ object NewTestamentBooks {
       Chapter(13, 37, Seq(21), ""),
       Chapter(14, 72, Seq(27, 54), ""),
       Chapter(15, 47, Seq(26), ""),
-      Chapter(16, 20, Seq(), "") //TODO check it
+      Chapter(16, 20, Seq(), "")
     )
   )
 
   //80,52,38,44,39,49,50,56,62,42,54,59,35,35,32,31,37,43,48,47,38,71,56,53
   val ntLuke = Book(Luke, Gospel, New,
     Seq(
-      Chapter(1, 80, Seq(), ""),
-      Chapter(2, 52, Seq(), ""),
+      Chapter(1, 80, Seq(21, 39, 57), ""),
+      Chapter(2, 52, Seq(25), ""),
       Chapter(3, 38, Seq(), ""),
-      Chapter(4, 44, Seq(), ""),
-      Chapter(5, 39, Seq(), ""),
-      Chapter(6, 49, Seq(), ""),
-      Chapter(7, 50, Seq(), ""),
-      Chapter(8, 56, Seq(), ""),
-      Chapter(9, 62, Seq(), ""),
-      Chapter(10, 42, Seq(), ""),
-      Chapter(11, 54, Seq(), ""),
-      Chapter(12, 59, Seq(), ""),
-      Chapter(13, 35, Seq(), ""),
-      Chapter(14, 35, Seq(), ""),
-      Chapter(15, 32, Seq(), ""),
+      Chapter(4, 44, Seq(31), ""),
+      Chapter(5, 39, Seq(17), ""),
+      Chapter(6, 49, Seq(27), ""),
+      Chapter(7, 50, Seq(31), ""),
+      Chapter(8, 56, Seq(26), ""),
+      Chapter(9, 62, Seq(18, 37), ""),
+      Chapter(10, 42, Seq(25), ""),
+      Chapter(11, 54, Seq(29), ""),
+      Chapter(12, 59, Seq(32), ""),
+      Chapter(13, 35, Seq(23), ""),
+      Chapter(14, 35, Seq(25), ""),
+      Chapter(15, 32, Seq(11), ""),
       Chapter(16, 31, Seq(), ""),
-      Chapter(17, 37, Seq(), ""),
-      Chapter(18, 43, Seq(), ""),
-      Chapter(19, 48, Seq(), ""),
-      Chapter(20, 47, Seq(), ""),
-      Chapter(21, 38, Seq(), ""),
-      Chapter(22, 71, Seq(), ""),
-      Chapter(23, 56, Seq(), ""),
-      Chapter(24, 53, Seq(), "")
+      Chapter(17, 37, Seq(20), ""),
+      Chapter(18, 43, Seq(24), ""),
+      Chapter(19, 48, Seq(28), ""),
+      Chapter(20, 47, Seq(27), ""),
+      Chapter(21, 38, Seq(20), ""),
+      Chapter(22, 71, Seq(21, 47), ""),
+      Chapter(23, 56, Seq(26), ""),
+      Chapter(24, 53, Seq(36), "")
     )
   )
 
   //51,25,36,54,47,71,53*,59*,41,42,57,50,38,31,27,33,26,40,42,31,25
   val ntJohn = Book(John, Gospel, New,
     Seq(
-      Chapter(1, 51, Seq(), ""),
+      Chapter(1, 51, Seq(29), ""),
       Chapter(2, 25, Seq(), ""),
-      Chapter(3, 36, Seq(), ""),
-      Chapter(4, 54, Seq(), ""),
-      Chapter(5, 47, Seq(), ""),
-      Chapter(6, 71, Seq(), ""),
-      Chapter(7, 53, Seq(), ""), //TODO check it
-      Chapter(8, 59, Seq(), ""), //TODO check it
-      Chapter(9, 41, Seq(), ""),
-      Chapter(10, 42, Seq(), ""),
-      Chapter(11, 57, Seq(), ""),
-      Chapter(12, 50, Seq(), ""),
-      Chapter(13, 38, Seq(), ""),
+      Chapter(3, 36, Seq(19), ""),
+      Chapter(4, 54, Seq(31), ""),
+      Chapter(5, 47, Seq(25), ""),
+      Chapter(6, 71, Seq(22, 45), ""),
+      Chapter(7, 53, Seq(28), ""), //TODO check it
+      Chapter(8, 59, Seq(28), ""), //TODO check it
+      Chapter(9, 41, Seq(24), ""),
+      Chapter(10, 42, Seq(24), ""),
+      Chapter(11, 57, Seq(30), ""),
+      Chapter(12, 50, Seq(27), ""),
+      Chapter(13, 38, Seq(21), ""),
       Chapter(14, 31, Seq(), ""),
       Chapter(15, 27, Seq(), ""),
       Chapter(16, 33, Seq(), ""),
       Chapter(17, 26, Seq(), ""),
-      Chapter(18, 40, Seq(), ""),
-      Chapter(19, 42, Seq(), ""),
+      Chapter(18, 40, Seq(19), ""),
+      Chapter(19, 42, Seq(23), ""),
       Chapter(20, 31, Seq(), ""),
       Chapter(21, 25, Seq(), "")
     )
@@ -122,32 +122,32 @@ object NewTestamentBooks {
   val ntActs = Book(Acts, Gospel, New,
     Seq(
       Chapter(1, 26, Seq(), ""),
-      Chapter(2, 47, Seq(), ""),
+      Chapter(2, 47, Seq(22), ""),
       Chapter(3, 26, Seq(), ""),
-      Chapter(4, 37, Seq(), ""),
-      Chapter(5, 42, Seq(), ""),
+      Chapter(4, 37, Seq(23), ""),
+      Chapter(5, 42, Seq(22), ""),
       Chapter(6, 15, Seq(), ""),
-      Chapter(7, 60, Seq(), ""),
-      Chapter(8, 40, Seq(), ""),
-      Chapter(9, 43, Seq(), ""),
-      Chapter(10, 48, Seq(), ""),
+      Chapter(7, 60, Seq(22, 44), ""),
+      Chapter(8, 40, Seq(26), ""),
+      Chapter(9, 43, Seq(22), ""),
+      Chapter(10, 48, Seq(24), ""),
       Chapter(11, 30, Seq(), ""),
       Chapter(12, 25, Seq(), ""),
-      Chapter(13, 52, Seq(), ""),
+      Chapter(13, 52, Seq(26), ""),
       Chapter(14, 28, Seq(), ""),
-      Chapter(15, 41, Seq(), ""),
-      Chapter(16, 40, Seq(), ""),
-      Chapter(17, 34, Seq(), ""),
+      Chapter(15, 41, Seq(22), ""),
+      Chapter(16, 40, Seq(22), ""),
+      Chapter(17, 34, Seq(16), ""),
       Chapter(18, 28, Seq(), ""),
-      Chapter(19, 40, Seq(), ""), //TODO check it
-      Chapter(20, 38, Seq(), ""),
-      Chapter(21, 40, Seq(), ""),
+      Chapter(19, 40, Seq(21), ""), //TODO check it
+      Chapter(20, 38, Seq(17), ""),
+      Chapter(21, 40, Seq(18), ""),
       Chapter(22, 30, Seq(), ""),
-      Chapter(23, 35, Seq(), ""),
+      Chapter(23, 35, Seq(16), ""),
       Chapter(24, 27, Seq(), ""),
       Chapter(25, 27, Seq(), ""),
       Chapter(26, 32, Seq(), ""),
-      Chapter(27, 44, Seq(), ""),
+      Chapter(27, 44, Seq(27), ""),
       Chapter(28, 31, Seq(), "")
     )
   )
@@ -162,14 +162,14 @@ object NewTestamentBooks {
       Chapter(5, 21, Seq(), ""),
       Chapter(6, 23, Seq(), ""),
       Chapter(7, 25, Seq(), ""),
-      Chapter(8, 39, Seq(), ""),
-      Chapter(9, 33, Seq(), ""),
+      Chapter(8, 39, Seq(22), ""),
+      Chapter(9, 33, Seq(16), ""),
       Chapter(10, 21, Seq(), ""),
-      Chapter(11, 36, Seq(), ""),
+      Chapter(11, 36, Seq(19), ""),
       Chapter(12, 21, Seq(), ""),
       Chapter(13, 14, Seq(), ""),
       Chapter(14, 23, Seq(), ""),
-      Chapter(15, 33, Seq(), ""),
+      Chapter(15, 33, Seq(14), ""),
       Chapter(16, 27, Seq(), "")
     )
   )
@@ -183,15 +183,15 @@ object NewTestamentBooks {
       Chapter(4, 21, Seq(), ""),
       Chapter(5, 13, Seq(), ""),
       Chapter(6, 20, Seq(), ""),
-      Chapter(7, 40, Seq(), ""),
+      Chapter(7, 40, Seq(20), ""),
       Chapter(8, 13, Seq(), ""),
       Chapter(9, 27, Seq(), ""),
-      Chapter(10, 33, Seq(), ""),
-      Chapter(11, 34, Seq(), ""),
+      Chapter(10, 33, Seq(19), ""),
+      Chapter(11, 34, Seq(17), ""),
       Chapter(12, 31, Seq(), ""),
       Chapter(13, 13, Seq(), ""),
-      Chapter(14, 40, Seq(), ""),
-      Chapter(15, 58, Seq(), ""),
+      Chapter(14, 40, Seq(21), ""),
+      Chapter(15, 58, Seq(29), ""),
       Chapter(16, 24, Seq(), "")
     )
   )
@@ -209,7 +209,7 @@ object NewTestamentBooks {
       Chapter(8, 24, Seq(), ""),
       Chapter(9, 15, Seq(), ""),
       Chapter(10, 18, Seq(), ""),
-      Chapter(11, 33, Seq(), ""),
+      Chapter(11, 33, Seq(16), ""),
       Chapter(12, 21, Seq(), ""),
       Chapter(13, 13, Seq(), "")
     )
@@ -234,7 +234,7 @@ object NewTestamentBooks {
       Chapter(2, 22, Seq(), ""),
       Chapter(3, 21, Seq(), ""),
       Chapter(4, 32, Seq(), ""),
-      Chapter(5, 33, Seq(), ""),
+      Chapter(5, 33, Seq(17), ""),
       Chapter(6, 24, Seq(), "")
     )
   )
--- src/test/scala/com/github/paniov/biread/test/BooksSpec.scala
+++ src/test/scala/com/github/paniov/biread/test/BooksSpec.scala
@@ -1,35 +1,46 @@
 package com.github.paniov.biread.test
 
 import com.github.paniov.biread.app.books.NewTestamentBooks._
-import com.github.paniov.biread.app.model.{Book}
+import com.github.paniov.biread.app.model.Book
 
-import scala.collection.SortedSet
+import scala.collection.{SortedSet, mutable}
 import scala.language.postfixOps
 
 class BooksSpec extends UnitSpec {
 
-  type BookMap = Map[Int, Any]
+  type BookMap = Map[Int, Int]
   type TupleInt = (Int, Int)
 
 
   def addOne: Int ⇒ Int = (1 + _)
 
-  def increaseIndex: ((Int, Int)) ⇒ (Int, Int) = (x: (Int, Int)) ⇒ (x._1, addOne(x._2))
+  def increaseIndex: ((Int, Int)) ⇒ (Int, Int) = x ⇒ (x._1, addOne(x._2))
 
-  def swapTuple: ((Int, Int)) ⇒ (Int, Int) = (x: (Int, Int)) ⇒ x.swap
+  def swapTuple: ((Int, Int)) ⇒ (Int, Int) = x ⇒ x.swap
 
-  def mapExpectedVerses: Seq[Int] ⇒ Map[Int, Int] = (x: Seq[Int]) ⇒ x.view.zipWithIndex.map(increaseIndex andThen swapTuple).toMap
+  def swapTupleSeq: ((Seq[Int], Int)) ⇒ (Int, Seq[Int]) = x ⇒ x.swap
+
+  def mapExpectedVerses: Seq[Int] ⇒ Map[Int, Int] = _.view.zipWithIndex.map(increaseIndex andThen swapTuple).toMap
 
   def mapActualVerses: Book ⇒ Map[Int, Int] = _.chapters.map(x ⇒ (x.chapter → x.verses)).toMap
 
+  def mapActualParts: Book ⇒ Map[Int, Seq[Int]] = _.chapters.map(x ⇒ (x.chapter → x.parts)).toMap
+
   def mapsEq(x: BookMap, y: BookMap): Boolean = (x.toSet diff y.toSet).toMap.isEmpty
+
+  def rejectEmpty(x: Map[Int, Seq[Int]]): Map[Int, Seq[Int]] = x.filter(_._2.nonEmpty)
+
+  def mapsDiff(x: Map[Int, Seq[Int]], y: Map[Int, Seq[Int]]): Boolean = (rejectEmpty(x).toSet diff rejectEmpty(y).toSet).toMap.isEmpty
+
+  def mapsPartsEq(x: Map[Int, Seq[Int]], y: Map[Int, Seq[Int]]): Boolean = mapsDiff(x, y) && mapsDiff(y, x)
 
   def mapKeysToSortedList: BookMap ⇒ List[Int] = SortedSet.empty[Int] ++ _.keys toList
 
   val versesMatt = Seq(25, 23, 17, 25, 48, 34, 29, 34, 38, 42, 30, 50, 58, 36, 39, 28, 27, 35, 30, 34, 46, 46, 39, 51, 46, 75, 66, 20)
-  val expectedMatt = mapExpectedVerses(versesMatt)
   val actualMatt = mapActualVerses(ntMatthew)
-  //  val actualPartsMatt =
+  val expectedMatt = mapExpectedVerses(versesMatt)
+
+  val actualPartsMatt = mapActualParts(ntMatthew)
   val expectedPartsMatt = Map(
     5 → Seq(27),
     6 → Seq(19),
@@ -63,16 +74,30 @@ class BooksSpec extends UnitSpec {
     assert(mapsEq(expectedMatt, actualMatt))
   }
 
-  //  it should "have a correct set of parts" in {
-  //    assert(mapsEq(partsMatt, partsMatt))
-  //  }
+  it should "have a correct set of parts" in {
+    assert(mapsPartsEq(expectedPartsMatt, actualPartsMatt))
+  }
 
   val versesMark = Seq(45, 28, 35, 41, 43, 56, 37, 38, 50, 52, 33, 44, 37, 72, 47, 20)
   val expectedMark = mapExpectedVerses(versesMark)
   val actualMark = mapActualVerses(ntMark)
-  //  val partsMark = Map(
-  //
-  //  )
+
+  val actualPartsMark = mapActualParts(ntMark)
+  val expectedPartsMark = Map(
+    1 → Seq(23),
+    3 → Seq(20),
+    4 → Seq(21),
+    5 → Seq(21),
+    6 → Seq(30),
+    8 → Seq(22),
+    9 → Seq(30),
+    10 → Seq(32),
+    11 → Seq(19),
+    12 → Seq(28),
+    13 → Seq(21),
+    14 → Seq(27, 54),
+    15 → Seq(26)
+  )
 
   "Mark" should "have 16 chapters" in {
     assert(actualMark.size == 16)
@@ -86,10 +111,40 @@ class BooksSpec extends UnitSpec {
     assert(mapsEq(expectedMark, actualMark))
   }
 
+  it should "have a corrects set of parts" in {
+    assert(mapsPartsEq(expectedPartsMark, actualPartsMark))
+  }
+
 
   val versesLuke = Seq(80, 52, 38, 44, 39, 49, 50, 56, 62, 42, 54, 59, 35, 35, 32, 31, 37, 43, 48, 47, 38, 71, 56, 53)
   val expectedLuke = mapExpectedVerses(versesLuke)
   val actualLuke = mapActualVerses(ntLuke)
+
+  val actualPartsLuke = mapActualParts(ntLuke)
+  val expectedPartsLuke = Map(
+    1 → Seq(21, 39, 57),
+    2 → Seq(25),
+    4 → Seq(31),
+    5 → Seq(17),
+    6 → Seq(27),
+    7 → Seq(31),
+    8 → Seq(26),
+    9 → Seq(18, 37),
+    10 → Seq(25),
+    11 → Seq(29),
+    12 → Seq(32),
+    13 → Seq(23),
+    14 → Seq(25),
+    15 → Seq(11),
+    17 → Seq(20),
+    18 → Seq(24),
+    19 → Seq(28),
+    20 → Seq(27),
+    21 → Seq(20),
+    22 → Seq(21, 47),
+    23 → Seq(26),
+    24 → Seq(36)
+  )
 
   "Luke" should "have 24 chapters" in {
     assert(actualLuke.size == 24)
@@ -103,10 +158,32 @@ class BooksSpec extends UnitSpec {
     assert(mapsEq(expectedLuke, actualLuke))
   }
 
+  it should "have a correct set of parts" in {
+    assert(mapsPartsEq(expectedPartsLuke, actualPartsLuke))
+  }
+
 
   val versesJohn = Seq(51, 25, 36, 54, 47, 71, 53, 59, 41, 42, 57, 50, 38, 31, 27, 33, 26, 40, 42, 31, 25)
   val expectedJohn = mapExpectedVerses(versesJohn)
   val actualJohn = mapActualVerses(ntJohn)
+
+  val actualPartsJohn = mapActualParts(ntJohn)
+  val expectedPartsJohn = Map(
+    1 → Seq(29),
+    3 → Seq(19),
+    4 → Seq(31),
+    5 → Seq(25),
+    6 → Seq(22, 45),
+    7 → Seq(28),
+    8 → Seq(28),
+    9 → Seq(24),
+    10 → Seq(24),
+    11 → Seq(30),
+    12 → Seq(27),
+    13 → Seq(21),
+    18 → Seq(19),
+    19 → Seq(23),
+  )
 
   "John" should "have 21 chapters" in {
     assert(actualJohn.size == 21)
@@ -120,10 +197,34 @@ class BooksSpec extends UnitSpec {
     assert(mapsEq(expectedJohn, actualJohn))
   }
 
+  it should "hve a correct set of parts" in {
+    assert(mapsPartsEq(expectedPartsJohn, actualPartsJohn))
+  }
+
 
   val versesActs = Seq(26, 47, 26, 37, 42, 15, 60, 40, 43, 48, 30, 25, 52, 28, 41, 40, 34, 28, 40, 38, 40, 30, 35, 27, 27, 32, 44, 31)
   val expectedActs = mapExpectedVerses(versesActs)
   val actualActs = mapActualVerses(ntActs)
+
+  val actualPartsActs = mapActualParts(ntActs)
+  val expectedPartsActs = Map(
+    2 → Seq(22),
+    4 → Seq(23),
+    5 → Seq(22),
+    7 → Seq(22, 44),
+    8 → Seq(26),
+    9 → Seq(22),
+    10 → Seq(24),
+    13 → Seq(26),
+    15 → Seq(22),
+    16 → Seq(22),
+    17 → Seq(16),
+    19 → Seq(21),
+    20 → Seq(17),
+    21 → Seq(18),
+    23 → Seq(16),
+    27 → Seq(27)
+  )
 
   "Acts" should "have 28 chapters" in {
     assert(actualActs.size == 28)
@@ -137,10 +238,22 @@ class BooksSpec extends UnitSpec {
     assert(mapsEq(expectedActs, actualActs))
   }
 
+  it should "have a correct set of parts" in {
+    assert(mapsPartsEq(expectedPartsActs, actualPartsActs))
+  }
+
 
   val versesRomans = Seq(32, 29, 31, 25, 21, 23, 25, 39, 33, 21, 36, 21, 14, 23, 33, 27)
   val expectedRomans = mapExpectedVerses(versesRomans)
   val actualRomans = mapActualVerses(ntRomans)
+
+  val actualPartsRomans = mapActualParts(ntRomans)
+  val expectedPartsRomans = Map(
+    8 → Seq(22),
+    9 → Seq(16),
+    11 → Seq(19),
+    15 → Seq(14)
+  )
 
   "Romans" should "have 16 chapters" in {
     assert(actualRomans.size == 16)
@@ -154,10 +267,23 @@ class BooksSpec extends UnitSpec {
     assert(mapsEq(expectedRomans, actualRomans))
   }
 
+  it should "have a correct set of parts" in {
+    assert(mapsPartsEq(expectedPartsRomans, actualPartsRomans))
+  }
+
 
   val versesCorinthians1 = Seq(31, 16, 23, 21, 13, 20, 40, 13, 27, 33, 34, 31, 13, 40, 58, 24)
   val expectedCorinthians1 = mapExpectedVerses(versesCorinthians1)
   val actualCorinthians1 = mapActualVerses(ntCorinthians1)
+
+  val actualPartsCorinthians1 = mapActualParts(ntCorinthians1)
+  val expectedPartsCorinthians1 = Map(
+    7 → Seq(20),
+    10 → Seq(19),
+    11 → Seq(17),
+    14 → Seq(21),
+    15 → Seq(29)
+  )
 
   "1 Corinthians" should "have 16 chapters" in {
     assert(actualCorinthians1.size == 16)
@@ -171,10 +297,19 @@ class BooksSpec extends UnitSpec {
     assert(mapsEq(expectedCorinthians1, actualCorinthians1))
   }
 
+  it should "have a correct set of parts" in {
+    assert(mapsPartsEq(expectedPartsCorinthians1, actualPartsCorinthians1))
+  }
+
 
   val versesCorinthians2 = Seq(24, 17, 18, 18, 21, 18, 16, 24, 15, 18, 33, 21, 13)
   val expectedCorinthians2 = mapExpectedVerses(versesCorinthians2)
   val actualCorinthians2 = mapActualVerses(ntCorinthians2)
+
+  val actualPartsCorinthians2 = mapActualParts(ntCorinthians2)
+  val expectedPartsCorinthians2 = Map(
+    11 → Seq(16)
+  )
 
   "2 Corinthians" should "have 13 chapters" in {
     assert(actualCorinthians2.size == 13)
@@ -188,10 +323,17 @@ class BooksSpec extends UnitSpec {
     assert(mapsEq(expectedCorinthians2, actualCorinthians2))
   }
 
+  it should "have a correct set of parts" in {
+    assert(mapsPartsEq(expectedPartsCorinthians2, actualPartsCorinthians2))
+  }
+
 
   val versesGalatians = Seq(24, 21, 29, 31, 26, 18)
   val expectedGalatians = mapExpectedVerses(versesGalatians)
   val actualGalatians = mapActualVerses(ntGalatians)
+
+  val actualPartsGalatians = mapActualParts(ntGalatians)
+  val expectedPartsGalatians = Map[Int, Seq[Int]]().empty
 
   "Galatians" should "have 6 chapters" in {
     assert(actualGalatians.size == 6)
@@ -205,10 +347,19 @@ class BooksSpec extends UnitSpec {
     assert(mapsEq(expectedGalatians, actualGalatians))
   }
 
+  it should "have a correct set of parts" in {
+    assert(mapsPartsEq(expectedPartsGalatians, actualPartsGalatians))
+  }
+
 
   val versesEphesians = Seq(23, 22, 21, 32, 33, 24)
   val expectedEphesians = mapExpectedVerses(versesEphesians)
   val actualEphesians = mapActualVerses(ntEphesians)
+
+  val actualPartsEphesians = mapActualParts(ntEphesians)
+  val expectedPartsEphesians = Map(
+    5 → Seq(17)
+  )
 
   "Ephesians" should "have 6 chapters" in {
     assert(actualEphesians.size == 6)
@@ -222,10 +373,17 @@ class BooksSpec extends UnitSpec {
     assert(mapsEq(expectedEphesians, actualEphesians))
   }
 
+  it should "have a correct set of parts" in {
+    assert(mapsPartsEq(expectedPartsEphesians, actualPartsEphesians))
+  }
+
 
   val versesPhilippians = Seq(30, 30, 21, 23)
   val expectedPhilippians = mapExpectedVerses(versesPhilippians)
   val actualPhilippians = mapActualVerses(ntPhilippians)
+
+  val actualPartsPhilippians = mapActualParts(ntPhilippians)
+  val expectedPartsPhilippians = Map[Int, Seq[Int]]().empty
 
   "Philippians" should "have 4 chapters" in {
     assert(actualPhilippians.size == 4)
@@ -239,10 +397,17 @@ class BooksSpec extends UnitSpec {
     assert(mapsEq(expectedPhilippians, actualPhilippians))
   }
 
+  it should "have a correct set of parts" in {
+    assert(mapsPartsEq(expectedPartsPhilippians, actualPartsPhilippians))
+  }
+
 
   val versesColossians = Seq(29, 23, 25, 18)
   val expectedColossians = mapExpectedVerses(versesColossians)
   val actualColossians = mapActualVerses(ntColossians)
+
+  val actualPartsColossians = mapActualParts(ntColossians)
+  val expectedPartsColossians = Map[Int, Seq[Int]]().empty
 
   "Colossians" should "have 4 chapters" in {
     assert(actualColossians.size == 4)
@@ -256,10 +421,16 @@ class BooksSpec extends UnitSpec {
     assert(mapsEq(expectedColossians, actualColossians))
   }
 
+  it should "have a correct set of parts" in {
+    assert(mapsPartsEq(expectedPartsColossians, actualPartsColossians))
+  }
+
 
   val versesThessalonians1 = Seq(10, 20, 13, 18, 28)
   val expectedThessalonians1 = mapExpectedVerses(versesThessalonians1)
   val actualThessalonians1 = mapActualVerses(ntThessalonians1)
+  val actualPartsThessalonians1 = mapActualParts(ntThessalonians1)
+  val expectedPartsThessalonians1 = Map[Int, Seq[Int]]().empty
 
   "1 Thessalonians" should "have 5 chapters" in {
     assert(actualThessalonians1.size == 5)
@@ -273,10 +444,16 @@ class BooksSpec extends UnitSpec {
     assert(mapsEq(expectedThessalonians1, actualThessalonians1))
   }
 
+  it should "have a correct set of parts" in {
+    assert(mapsPartsEq(expectedPartsThessalonians1, actualPartsThessalonians1))
+  }
+
 
   val versesThessalonians2 = Seq(12, 17, 18)
   val expectedThessalonians2 = mapExpectedVerses(versesThessalonians2)
   val actualThessalonians2 = mapActualVerses(ntThessalonians2)
+  val actualPartsThessalonians2 = mapActualParts(ntThessalonians2)
+  val expectedPartsThessalonians2 = Map[Int, Seq[Int]]().empty
 
   "2 Thessalonians" should "have 3 chapters" in {
     assert(actualThessalonians2.size == 3)
@@ -290,10 +467,16 @@ class BooksSpec extends UnitSpec {
     assert(mapsEq(expectedThessalonians2, actualThessalonians2))
   }
 
+  it should "have a correct set of parts" in {
+    assert(mapsPartsEq(expectedPartsThessalonians2, actualPartsThessalonians2))
+  }
+
 
   val versesTimothy1 = Seq(20, 15, 16, 16, 25, 21)
   val expectedTimothy1 = mapExpectedVerses(versesTimothy1)
   val actualTimothy1 = mapActualVerses(ntTimothy1)
+  val actualPartsTimothy1 = mapActualParts(ntTimothy1)
+  val expectedPartsTimothy1 = Map[Int, Seq[Int]]().empty
 
   "1 Timothy" should "have 6 chapters" in {
     assert(actualTimothy1.size == 6)
@@ -307,10 +490,16 @@ class BooksSpec extends UnitSpec {
     assert(mapsEq(expectedTimothy1, actualTimothy1))
   }
 
+  it should "have a correct set of parts" in {
+    assert(mapsPartsEq(expectedPartsTimothy1, actualPartsTimothy1))
+  }
+
 
   val versesTimothy2 = Seq(18, 26, 17, 22)
   val expectedTimothy2 = mapExpectedVerses(versesTimothy2)
   val actualTimothy2 = mapActualVerses(ntTimothy2)
+  val actualPartsTimothy2 = mapActualParts(ntTimothy2)
+  val expectedPartsTimothy2 = Map[Int, Seq[Int]]().empty
 
   "2 Timothy" should "have 4 chapters" in {
     assert(actualTimothy2.size == 4)
@@ -324,10 +513,16 @@ class BooksSpec extends UnitSpec {
     assert(mapsEq(expectedTimothy2, actualTimothy2))
   }
 
+  it should "have a correct set of parts" in {
+    assert(mapsPartsEq(expectedPartsTimothy2, actualPartsTimothy2))
+  }
+
 
   val versesTitus = Seq(6, 15, 15)
   val expectedTitus = mapExpectedVerses(versesTitus)
   val actualTitus = mapActualVerses(ntTitus)
+  val actualPartsTitus = mapActualParts(ntTitus)
+  val expectedPartsTitus = Map[Int, Seq[Int]]().empty
 
   "Titus" should "have 3 chapters" in {
     assert(actualTitus.size == 3)
@@ -341,10 +536,17 @@ class BooksSpec extends UnitSpec {
     assert(mapsEq(expectedTitus, actualTitus))
   }
 
+  it should "have a correct set of parts" in {
+    assert(mapsPartsEq(expectedPartsTitus, actualPartsTitus))
+  }
+
 
   val versesPhilemon = Seq(25)
   val expectedPhilemon = mapExpectedVerses(versesPhilemon)
   val actualPhilemon = mapActualVerses(ntPhilemon)
+  val actualPartsPhilemon = mapActualParts(ntPhilemon)
+  val expectedPartsPhilemon = Map[Int, Seq[Int]]().empty
+
 
   "Philemon" should "have 1 chapters" in {
     assert(actualPhilemon.size == 1)
@@ -358,10 +560,16 @@ class BooksSpec extends UnitSpec {
     assert(mapsEq(expectedPhilemon, actualPhilemon))
   }
 
+  it should "have a correct set of parts" in {
+    assert(mapsPartsEq(expectedPartsPhilemon, actualPartsPhilemon))
+  }
+
 
   val versesHebrews = Seq(14, 18, 19, 16, 14, 20, 28, 13, 28, 39, 40, 29, 25)
   val expectedHebrews = mapExpectedVerses(versesHebrews)
   val actualHebrews = mapActualVerses(ntHebrews)
+  val actualPartsHebrews = mapActualParts(ntHebrews)
+  val expectedPartsHebrews = Map[Int, Seq[Int]]().empty
 
   "Hebrews" should "have 13 chapters" in {
     assert(actualHebrews.size == 13)
@@ -375,10 +583,16 @@ class BooksSpec extends UnitSpec {
     assert(mapsEq(expectedHebrews, actualHebrews))
   }
 
+  it should "have a correct set of parts" in {
+    assert(mapsPartsEq(expectedPartsHebrews, actualPartsHebrews))
+  }
+
 
   val versesJames = Seq(27, 26, 18, 17, 20)
   val expectedJames = mapExpectedVerses(versesJames)
   val actualJames = mapActualVerses(ntJames)
+  val actualPartsJames = mapActualParts(ntJames)
+  val expectedPartsJames = Map[Int, Seq[Int]]().empty
 
   "James" should "have 5 chapters" in {
     assert(actualJames.size == 5)
@@ -392,10 +606,16 @@ class BooksSpec extends UnitSpec {
     assert(mapsEq(expectedJames, actualJames))
   }
 
+  it should "have a correct set of parts" in {
+    assert(mapsPartsEq(expectedPartsJames, actualPartsJames))
+  }
+
 
   val versesPeter1 = Seq(25, 25, 22, 19, 14)
   val expectedPeter1 = mapExpectedVerses(versesPeter1)
   val actualPeter1 = mapActualVerses(ntPeter1)
+  val actualPartsPeter1 = mapActualParts(ntPeter1)
+  val expectedPartsPeter1 = Map[Int, Seq[Int]]().empty
 
   "1 Peter" should "have 5 chapters" in {
     assert(actualPeter1.size == 5)
@@ -409,10 +629,16 @@ class BooksSpec extends UnitSpec {
     assert(mapsEq(expectedPeter1, actualPeter1))
   }
 
+  it should "have a correct set of parts" in {
+    assert(mapsPartsEq(expectedPartsPeter1, actualPartsPeter1))
+  }
+
 
   val versesPeter2 = Seq(21, 22, 18)
   val expectedPeter2 = mapExpectedVerses(versesPeter2)
   val actualPeter2 = mapActualVerses(ntPeter2)
+  val actualPartsPeter2 = mapActualParts(ntPeter2)
+  val expectedPartsPeter2 = Map[Int, Seq[Int]]().empty
 
   "2 Peter" should "have 3 chapters" in {
     assert(actualPeter2.size == 3)
@@ -426,10 +652,15 @@ class BooksSpec extends UnitSpec {
     assert(mapsEq(expectedPeter2, actualPeter2))
   }
 
+  it should "have a correct set of parts" in {
+    assert(mapsPartsEq(expectedPartsPeter2, actualPartsPeter2))
+  }
 
   val versesJohn1 = Seq(10, 29, 24, 21, 21)
   val expectedJohn1 = mapExpectedVerses(versesJohn1)
   val actualJohn1 = mapActualVerses(ntJohn1)
+  val actualPartsJohn1 = mapActualParts(ntJohn1)
+  val expectedPartsJohn1 = Map[Int, Seq[Int]]().empty
 
   "1 John" should "have 5 chapters" in {
     assert(actualJohn1.size == 5)
@@ -443,10 +674,15 @@ class BooksSpec extends UnitSpec {
     assert(mapsEq(expectedJohn1, actualJohn1))
   }
 
+  it should "have a correct set of parts" in {
+    assert(mapsPartsEq(expectedPartsJohn1, actualPartsJohn1))
+  }
 
   val versesJohn2 = Seq(13)
   val expectedJohn2 = mapExpectedVerses(versesJohn2)
   val actualJohn2 = mapActualVerses(ntJohn2)
+  val actualPartsJohn2 = mapActualParts(ntJohn2)
+  val expectedPartsJohn2 = Map[Int, Seq[Int]]().empty
 
   "2 John" should "have 1 chapters" in {
     assert(actualJohn2.size == 1)
@@ -460,10 +696,16 @@ class BooksSpec extends UnitSpec {
     assert(mapsEq(expectedJohn2, actualJohn2))
   }
 
+  it should "have a correct set of parts" in {
+    assert(mapsPartsEq(expectedPartsJohn2, actualPartsJohn2))
+  }
+
 
   val versesJohn3 = Seq(15)
   val expectedJohn3 = mapExpectedVerses(versesJohn3)
   val actualJohn3 = mapActualVerses(ntJohn3)
+  val actualPartsJohn3 = mapActualParts(ntJohn3)
+  val expectedPartsJohn3 = Map[Int, Seq[Int]]().empty
 
   "3 John" should "have 1 chapters" in {
     assert(actualJohn3.size == 1)
@@ -477,10 +719,16 @@ class BooksSpec extends UnitSpec {
     assert(mapsEq(expectedJohn3, actualJohn3))
   }
 
+  it should "have a correct set of parts" in {
+    assert(mapsPartsEq(expectedPartsJohn3, actualPartsJohn3))
+  }
+
 
   val versesJude = Seq(25)
   val expectedJude = mapExpectedVerses(versesJude)
   val actualJude = mapActualVerses(ntJude)
+  val actualPartsJude = mapActualParts(ntJude)
+  val expectedPartsJude = Map[Int, Seq[Int]]().empty
 
   "Jude" should "have 1 chapters" in {
     assert(actualJude.size == 1)
@@ -494,10 +742,16 @@ class BooksSpec extends UnitSpec {
     assert(mapsEq(expectedJude, actualJude))
   }
 
+  it should "have a correct set of parts" in {
+    assert(mapsPartsEq(expectedPartsJude, actualPartsJude))
+  }
+
 
   val versesRevelation = Seq(20, 29, 22, 11, 14, 17, 17, 13, 21, 11, 19, 17, 18, 20, 8, 21, 18, 24, 21, 15, 27, 21)
   val expectedRevelation = mapExpectedVerses(versesRevelation)
   val actualRevelation = mapActualVerses(ntRevelation)
+  val actualPartsRevelation = mapActualParts(ntRevelation)
+  val expectedPartsRevelation = Map[Int, Seq[Int]]().empty
 
   "Revelation" should "have 22 chapters" in {
     assert(actualRevelation.size == 22)
@@ -511,6 +765,9 @@ class BooksSpec extends UnitSpec {
     assert(mapsEq(expectedRevelation, actualRevelation))
   }
 
+  it should "have a correct set of parts" in {
+    assert(mapsPartsEq(expectedPartsRevelation, actualPartsRevelation))
+  }
 
 }
 
